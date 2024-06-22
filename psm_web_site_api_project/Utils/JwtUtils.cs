dbbd538aa8045d677c6f45770f97b64f4d8b90b8
luiser8 <leduardo.rondon@gmail.com>
2024-06-21 22:32:21 -0400

    Code format

--- psm_web_site_api_project/Utils/JwtUtils.cs
+++ psm_web_site_api_project/Utils/JwtUtils.cs
@@ -6,99 +6,99 @@ using Microsoft.IdentityModel.Tokens;
 using psm_web_site_api_project.Dto;
 
 namespace psm_web_site_api_project.Utils.JwtUtils;
-    public static class JwtUtils
+public static class JwtUtils
+{
+    private static IConfiguration? configuration;
+
+    public static IConfiguration Configuration
     {
-        private static IConfiguration configuration;
-
-        public static IConfiguration Configuration
+        get
         {
-            get
-            {
-                var builder = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json");
-                configuration = builder.Build();
-                return configuration;
-            }
-        }
-
-        public static string GetSetting()
-        {
-            return Configuration.GetSection("AppSettings:Token").Value;
-        }
-
-        public static string CreateToken(TokenDto tokenDto)
-        {
-            var claims = new List<Claim>();
-
-            claims.Add(new Claim("iduser", tokenDto.IdUsuario.ToString()));
-            claims.Add(new Claim("firstname", tokenDto.Nombres));
-            claims.Add(new Claim("lastname", tokenDto.Apellidos));
-            claims.Add(new Claim(ClaimTypes.Email, tokenDto.Correo));
-
-            foreach (var rol in tokenDto.Roles)
-            {
-                claims.Add(new Claim("Rol", rol?.IdRol + "-" + rol.Nombre));
-            }
-
-            foreach (var extension in tokenDto.Extension)
-            {
-                claims.Add(new Claim("Extension", extension.IdExtension + "-" + extension.Nombre));
-            }
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetSetting()));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var jwt = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds);
-
-            var accessToken = new JwtSecurityTokenHandler().WriteToken(jwt);
-
-            return accessToken;
-        }
-
-        public static string RefreshToken(TokenDto tokenDto)
-        {
-            var claims = new List<Claim>();
-
-            claims.Add(new Claim("iduser", tokenDto.IdUsuario.ToString()));
-            claims.Add(new Claim("firstname", tokenDto.Nombres));
-            claims.Add(new Claim("lastname", tokenDto.Apellidos));
-            claims.Add(new Claim(ClaimTypes.Email, tokenDto.Correo));
-
-            foreach (var rol in tokenDto.Roles)
-            {
-                claims.Add(new Claim("Rol", rol?.IdRol + "-" + rol.Nombre));
-            }
-
-            foreach (var extension in tokenDto.Extension)
-            {
-                claims.Add(new Claim("Extension", extension.IdExtension + "-" + extension.Nombre));
-            }
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetSetting()));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var jwt = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(7),
-                signingCredentials: creds);
-
-            var refreshToken = new JwtSecurityTokenHandler().WriteToken(jwt);
-
-            return refreshToken;
-        }
-
-        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
-            using (var hmac = new HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            configuration = builder.Build();
+            return configuration;
         }
     }
+
+    public static string GetSetting()
+    {
+        return Configuration.GetSection("AppSettings:Token").Value;
+    }
+
+    public static string CreateToken(TokenDto tokenDto)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim("iduser", tokenDto.IdUsuario.ToString()),
+            new Claim("firstname", tokenDto.Nombres),
+            new Claim("lastname", tokenDto.Apellidos),
+            new Claim(ClaimTypes.Email, tokenDto.Correo)
+        };
+
+        foreach (var rol in tokenDto.Roles)
+        {
+            claims.Add(new Claim("Rol", rol?.IdRol + "-" + rol.Nombre));
+        }
+
+        foreach (var extension in tokenDto.Extension)
+        {
+            claims.Add(new Claim("Extension", extension.IdExtension + "-" + extension.Nombre));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetSetting()));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var jwt = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds);
+
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+        return accessToken;
+    }
+
+    public static string RefreshToken(TokenDto tokenDto)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim("iduser", tokenDto.IdUsuario.ToString()),
+            new Claim("firstname", tokenDto.Nombres),
+            new Claim("lastname", tokenDto.Apellidos),
+            new Claim(ClaimTypes.Email, tokenDto.Correo)
+        };
+
+        foreach (var rol in tokenDto.Roles)
+        {
+            claims.Add(new Claim("Rol", rol?.IdRol + "-" + rol.Nombre));
+        }
+
+        foreach (var extension in tokenDto.Extension)
+        {
+            claims.Add(new Claim("Extension", extension.IdExtension + "-" + extension.Nombre));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetSetting()));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var jwt = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(7),
+            signingCredentials: creds);
+
+        var refreshToken = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+        return refreshToken;
+    }
+
+    public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+    }
+}
--- psm_web_site_api_project/Utils/MD5Utils.cs
+++ psm_web_site_api_project/Utils/MD5Utils.cs
@@ -1,19 +1,18 @@
 using System.Text;
 
 namespace psm_web_site_api_project.Utils.Md5utils;
-    public static class Md5utilsClass
+public static class Md5utilsClass
+{
+    private static System.Security.Cryptography.MD5 md5String;
+
+    public static string GetMD5(string password)
     {
-        private static System.Security.Cryptography.MD5 md5String;
+        md5String = System.Security.Cryptography.MD5.Create();
+        ASCIIEncoding encoding = new ASCIIEncoding();
+        StringBuilder stringBuilder = new StringBuilder();
+        byte[] stream = md5String.ComputeHash(encoding.GetBytes(password));
 
-        public static string GetMD5(string password)
-        {
-            md5String = System.Security.Cryptography.MD5.Create();
-            ASCIIEncoding encoding = new ASCIIEncoding();
-            byte[] stream = null;
-            StringBuilder stringBuilder = new StringBuilder();
-            stream = md5String.ComputeHash(encoding.GetBytes(password));
-
-            for (int i = 0; i < stream.Length; i++) stringBuilder.AppendFormat("{0:x2}", stream[i]);
-            return stringBuilder.ToString();
-        }
+        for (int i = 0; i < stream.Length; i++) stringBuilder.AppendFormat("{0:x2}", stream[i]);
+        return stringBuilder.ToString();
     }
+}
